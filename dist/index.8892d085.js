b98f49110ae65481e6c32fbc958995df70851976
simonpatrickreyes <simonpatrickreyes@gmail.com>
2022-12-07 13:28:10 +0100

    Empezando Threads

---
+++ dist/index.8892d085.js
@@ -0,0 +1,7 @@
+const toggleMenuElement = document.getElementById("toggle-menu");
+const mainMenuElement = document.getElementById("main-menu");
+toggleMenuElement.addEventListener("click", ()=>{
+    mainMenuElement.classList.toggle("main-menu--show");
+});
+
+//# sourceMappingURL=index.8892d085.js.map
