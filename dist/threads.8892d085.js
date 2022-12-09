ee550e0d8bde4127e91452d008700e3bdff2b1c6
simonpatrickreyes <simonpatrickreyes@gmail.com>
2022-12-09 12:42:29 +0100

    Avances 09/12/22

--- dist/threads.8892d085.js
+++ dist/threads.8892d085.js
@@ -1,11 +1,13 @@
-const toggleMenuElement = document.getElementById("toggle-menu");
-const mainMenuElement = document.getElementById("main-menu");
-toggleMenuElement.addEventListener("click", ()=>{
-    if (mainMenuElement.style.display === "none") mainMenuElement.style.display = "block";
-    else mainMenuElement.style.display = "none";
-}); /*toggleMenuElement.addEventListener('click', () => {
+window.onload = ()=>{
+    const toggleMenuElement = document.getElementById("toggle-menu");
+    const mainMenuElement = document.getElementById("main-menu");
+    toggleMenuElement.addEventListener("click", ()=>{
+        if (mainMenuElement.style.display === "none") mainMenuElement.style.display = "block";
+        else mainMenuElement.style.display = "none";
+    });
+} /*toggleMenuElement.addEventListener('click', () => {
     console.log("hola")
     mainMenuElement.classList.toggle('main-menu--show');
-});*/ 
+});*/ ;
 
-//# sourceMappingURL=threads.8892d085.js.map
+//# sourceMappingURL=characters.8892d085.js.map
--- src/asset/jscript/scripts.js
+++ src/asset/jscript/scripts.js
@@ -1,15 +1,16 @@
-const toggleMenuElement = document.getElementById('toggle-menu');
-const mainMenuElement = document.getElementById('main-menu');
+window.onload = () => {
+    const toggleMenuElement = document.getElementById('toggle-menu');
+    const mainMenuElement = document.getElementById('main-menu');
 
-toggleMenuElement.addEventListener('click', () => {
-    
-    if (mainMenuElement.style.display === "none") {
+    toggleMenuElement.addEventListener('click', () => {
+        display = getComputedStyle(e.target).display;
+        if (display === "none") {
             mainMenuElement.style.display = "block";
-          } else {
-                mainMenuElement.style.display = "none";
-          }
-});
-
+        } else {
+            mainMenuElement.style.display = "none";
+        }
+    });
+}
 
 
 /*toggleMenuElement.addEventListener('click', () => {
